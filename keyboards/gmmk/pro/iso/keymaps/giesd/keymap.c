516cd9dea3dae2bed4b7f2d0da06fd1f969a7863
Daniel <d.gies@gmx.de.de>
2021-07-26 20:36:38 +0200

    Removed some RGB funcions. Added dynamic macros.

--- keyboards/gmmk/pro/iso/keymaps/giesd/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/giesd/keymap.c
@@ -17,6 +17,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include "rgb_matrix_map.h"
 
+enum {
+    DEFAULT_LAYER,
+    NUMBLOCK_LAYER,
+    MACRO_LAYER,
+    FN_LAYER
+};
+
+bool isRecordingMacro = false;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
@@ -34,26 +43,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //
     // To put the keyboard in bootloader mode, use FN+backspace. If you accidentally put it into bootloader, you can just unplug the USB cable and
     // it'll be back to normal when you plug it back in.
-    [0] = LAYOUT(
+    [DEFAULT_LAYER] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MPLY,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_PGUP,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGDN,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(3),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
-    [1] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, _______, _______, RESET,            _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   RGB_MOD,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          RGB_RMOD,
-        _______, _______, _______, _______, _______, _______, _______, TO(2),   _______, _______, _______, _______,          _______, RGB_VAI, _______,
-        _______, _______, _______,                            _______,                            _______, _______, _______, RGB_HUD, RGB_VAD, RGB_HUI
-    ),
-
-    [2] = LAYOUT(
-        TO(0),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+    [NUMBLOCK_LAYER] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______, _______,                   _______,
         _______, _______, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, _______, _______,          _______,
@@ -61,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
     ),
 
-    [3] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+    [MACRO_LAYER] = LAYOUT(
+        _______, DM_REC1, DM_REC2, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
@@ -70,11 +70,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
     ),
 
+    [FN_LAYER] = LAYOUT(
+        TO(0),   DM_PLY1, DM_PLY2, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MSTP, KC_MPRV, KC_MNXT, KC_PSCR,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, _______, _______, RESET,            _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, TG(1),   TG(2),   _______, _______, _______,          _______, RGB_VAI, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, RGB_VAD, _______
+    ),
+
 };
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    rgb_matrix_disable();
     if (clockwise) {
       tap_code(KC_VOLU);
     } else {
@@ -85,9 +93,46 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 #endif
 
 #ifdef RGB_MATRIX_ENABLE
-void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+void rgb_matrix_indicators_kb(void) {
     if (IS_HOST_LED_ON(USB_LED_CAPS_LOCK)) {
-        rgb_matrix_set_color(LED_CAPS, RGB_RED);
+        for (uint8_t i = 0; i < sizeof(LED_SIDE_LEFT); i++) {
+            rgb_matrix_set_color(LED_SIDE_LEFT[i], RGB_RED);
+        }
     }
+    if (IS_LAYER_ON(NUMBLOCK_LAYER)) {
+        for (uint8_t i = 0; i < sizeof(LED_LIST_NUMBLOCK); i++) {
+            rgb_matrix_set_color(LED_LIST_NUMBLOCK[i], RGB_WHITE);
+        }
+    }
+    if (IS_LAYER_ON(MACRO_LAYER)) {
+        rgb_matrix_set_color(LED_F1, RGB_GREEN);
+        rgb_matrix_set_color(LED_F2, RGB_GREEN);
+    }
+    if (isRecordingMacro) {
+        rgb_matrix_set_color(LED_ESC, RGB_RED);
+    }
+}
+
+void suspend_power_down_user(void) {
+    rgb_matrix_set_suspend_state(true);
+}
+
+void suspend_wakeup_init_user(void) {
+    rgb_matrix_set_suspend_state(false);
+}
+
+void keyboard_post_init_user(void) {
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+    rgb_matrix_sethsv_noeeprom(0, 255, 0);
+}
+#endif
+
+#ifdef DYNAMIC_MACRO_ENABLE
+void dynamic_macro_record_start_user(void) {
+    isRecordingMacro = true;
+}
+
+void dynamic_macro_record_end_user(int8_t direction) {
+    isRecordingMacro = false;
 }
 #endif
